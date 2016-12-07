647946c411aa5f0163c3be7618c6160a0b94f661
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2016-12-07 09:41:57 +0100

    additional nodes for storing the data on the server

---
+++ nodes_py/pointing_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from hmi_interpreter.srv import *
+from geometry_msgs.msg import Pose
+import rospy
+import sys
+
+pose=Pose()
+
+def set_pointer(req):
+    global pose
+    print "Returning: "
+    multi = req.pointing
+    if req.action.data == "set":
+        pose.position.x = multi.position.x
+        pose.position.y = multi.position.y
+        pose.position.z = multi.position.z
+        pose.orientation.x = multi.orientation.x
+        pose.orientation.y = multi.orientation.y
+        pose.orientation.z = multi.orientation.z
+        pose.orientation.w = multi.orientation.w
+    else:
+        print pose
+    return pointerResponse(pose)
+
+def add_pointing_server():
+    rospy.init_node('add_pointing_server')
+    s = rospy.Service('pointing_server', pointer, set_pointer)
+    print "Ready to add pointing gesture."
+    rospy.spin()
+
+def handle_add_three_ints(req):
+    print "Returning: "
+    print req
+    return text_parserResponse("test done2")
+
+
+if __name__ == "__main__":
+    add_pointing_server()
---
+++ nodes_py/pointing_sub.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('hmi_interpreter')
+from hmi_interpreter.srv import *
+from geometry_msgs.msg import *
+from std_msgs.msg import *
+import rospy
+import re
+import sys
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import string 
+import os
+import commands
+from std_msgs.msg import String
+
+def call_client(pose):
+    string = String()
+    string.data = "set"
+    rospy.wait_for_service("pointing_server")
+    try: 
+        pointing_server = rospy.ServiceProxy("pointing_server", pointer)
+        resp1 = pointing_server(string,pose)
+        result = resp1.result
+        return result
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def subscriberCB(data):
+    posy = data.pose
+    result = call_client(posy)
+    
+def start_function():
+    rospy.init_node("start_pointing_sub")
+    rospy.Subscriber("pointing_gesture", PoseStamped, subscriberCB)
+    print "Ready for start_function with Subscriber"
+    rospy.spin()
+
+
+
+if __name__ == "__main__":
+     start_function()
