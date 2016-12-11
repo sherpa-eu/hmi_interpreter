7792a95a5c28cced9fea2a2b78bda14c28ec0889
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2016-12-11 13:39:50 +0100

    added new node for gui

---
+++ nodes_py/gui.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+from Tkinter import *
+import rospy
+from std_msgs.msg import String
+from instructor_mission.msg import protocol_dialogue
+from instructor_mission.srv import call_cmd
+import time
+import sys
+
+checker="false"
+def task(entry_text):
+   print 'texte'
+
+
+def goTo_subscriber():
+    #writing a subscriber and publishing info
+    #check for switch
+    # if switch then change_image_field
+
+
+def change_image_field():
+   global checker
+   if checker == "false":
+      checker = "true"
+      b1.config(image=on)
+   else:
+      checker = "false"
+      b1.config(image=off)
+
+   if checker == "true":
+       goTo_subscriber()
+
+
+
+
+def show_entry_fields():
+   global var
+   if len(e1.get("1.0", "end-1c")) == 0:
+      window.insert(INSERT,'Please give a command!\n','rotcolor')
+   else:
+      entry_text = e1.get("1.0",END)
+      e1.delete("1.0",END)
+      window.insert(INSERT,'Genius:  ','hcolor')
+      window.insert(END,entry_text+'','hnbcolor')
+
+master = Tk()
+master.title("Dialogue Interface")
+var = 0
+#size of the window
+window = Text(master, height=40, width=70)
+window.tag_configure('big', font=('Verdana',20,'bold'))
+scroll = Scrollbar(master, command=window.yview)
+window.tag_configure('big', font=('Verdana',20,'bold'))
+window.tag_configure('hcolor', foreground='#476042', 
+                      font=('Tempus Sans ITC', 12, 'bold'))
+window.tag_configure('hnbcolor', foreground='#476042', 
+                      font=('Tempus Sans ITC', 12, 'italic'))
+window.tag_configure('rotcolor', foreground='#EF4423', 
+                      font=('Tempus Sans ITC', 12, 'bold'))
+
+window.tag_config('coordinate',borderwidth=100)
+window.grid(row=4, columnspan = 1)
+
+dialog_label = Label(master)
+e1 = Text(master, width=45, height=2)
+
+#mic
+b1 = Button(master, command=change_image_field)
+e1.grid(row=1, column=0, pady=4, padx=4)
+b1.grid(row=4, column=1,sticky=W, pady=4, padx=4)
+mi = PhotoImage(file="../img/speaker_off.png")
+off = mi.subsample(5,5)
+b1.config(image=off)
+mis = PhotoImage(file="../img/speaker_on.png")
+on = mis.subsample(5,5)
+
+
+Button(master, text='Quit', font=('Arial', 12,'bold', 'italic'), foreground='#ff8000',command=master.quit).grid(row=5, column=1,sticky=W, padx=16)
+Button(master, text='Enter', font=('Arial', 12,'bold', 'italic'),command=show_entry_fields).grid(row=1, column=0, sticky=W, pady=4, padx=4)
+
+mainloop( )
