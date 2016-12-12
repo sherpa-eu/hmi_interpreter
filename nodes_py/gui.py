7fdc1d59d6394e8173d3dadb65a6625a2b929e1e
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2016-12-12 14:34:48 +0100

    added new vocabular in order and added functionality to gui

--- nodes_py/gui.py
+++ nodes_py/gui.py
@@ -6,18 +6,35 @@ from instructor_mission.msg import protocol_dialogue
 from instructor_mission.srv import call_cmd
 import time
 import sys
+import rospkg
 
 checker="false"
 
+
+def func(event):
+   print "return "
+   e1.delete("end-1c",END)
+   show_entry_fields()
+
 def publisher_callback(data):
+   print "publisher"
    if checker == "true":
       if data.data != "SWITCH":
-         pub = rospy.Publisher('internal/recognizer/output', String, queue_size=10)
-         pub.publish(data)
+         result = data.data
+         print result
+         print "mittig"
+         result.replace("\n","")
+         print result
+         print "result: "
+         string = String()
+         string.data = result
+         pub = rospy.Publisher('/internal/recognizer/output', String, queue_size=10)
+         pub.publish(result)
       else:
          change_image_field()
 
 def change_image_field():
+   print "field"
    global checker
    if checker == "false":
       checker = "true"
@@ -27,14 +44,27 @@ def change_image_field():
       b1.config(image=off)
 
 def show_entry_fields():
-   if len(e1.get("1.0", "end-1c")) == 0:
+   print "show"
+   print len(e1.get("1.0","0.0"))
+   print len(e1.get("0.0",END))
+   print len(e1.get("end-1c",END))
+   print len(e1.get("1.0", "end-1c"))
+   if len(e1.get("1.0", "end-1c")) == 0 or  len(e1.get("1.0", "end-1c")) == 1:
       window.insert(INSERT,'Please give a command!\n','rotcolor')
+      e1.delete("1.0","end-1c")
    else:
-      entry_text = e1.get("1.0",END)
-      e1.delete("1.0",END)
+      entry_text = e1.get("1.0","end-1c")
+      e1.delete("1.0","end-1c")
       window.insert(INSERT,'Genius:  ','hcolor')
-      window.insert(END,entry_text+'','hnbcolor')
-
+      window.insert(END,entry_text+'\n','hnbcolor')
+      print entry_text
+      print "mittig"
+      entry_text.replace("\n","")
+      print entry_text
+      string = String()
+      string.data = entry_text
+      pub = rospy.Publisher('/internal/recognizer/output', String, queue_size=10)
+      pub.publish(string)
 
 
 if __name__ == "__main__":
@@ -58,17 +88,22 @@ if __name__ == "__main__":
    dialog_label = Label(master)
    e1 = Text(master, width=45, height=2)
    
+   #package path
+   rospack = rospkg.RosPack()
+   #rospack.list_pkgs() 
+   path = rospack.get_path('hmi_interpreter')
+   path = path+"/img"
    #mic
    b1 = Button(master, command=change_image_field)
    e1.grid(row=1, column=0, pady=4, padx=4)
    b1.grid(row=4, column=1,sticky=W, pady=4, padx=4)
-   mi = PhotoImage(file="../img/speaker_off.png")
+   mi = PhotoImage(file=path+"/speaker_off.png")
    off = mi.subsample(5,5)
    b1.config(image=off)
-   mis = PhotoImage(file="../img/speaker_on.png")
+   mis = PhotoImage(file=path+"/speaker_on.png")
    on = mis.subsample(5,5)
    
-   
+   master.bind('<Return>',func)
    Button(master, text='Quit', font=('Arial', 12,'bold', 'italic'), foreground='#ff8000',command=master.quit).grid(row=5, column=1,sticky=W, padx=16)
    Button(master, text='Enter', font=('Arial', 12,'bold', 'italic'),command=show_entry_fields).grid(row=1, column=0, sticky=W, pady=4, padx=4)
    rospy.Subscriber("recognizer/output", String, publisher_callback)
